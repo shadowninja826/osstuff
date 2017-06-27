14a1d4a537f8891b6e423bacaabf7c6de582c19f
shadowninja826 <sodachheng@gmail.com>
2017-06-27 07:39:13 -0500

    change the output file name
    appended the current date to the output file

--- status_cap.sh
+++ status_cap.sh
@@ -4,47 +4,47 @@
 # information into a file located in the working directory
 # Soda Chheng June 26 2017
 
-
-clock >> output.txt
+todayoutput=$(date +"%Y%m%d")_output.txt
+clock >> $todayoutput
 wait
 #commands to obtain the networking information
-echo -e "\n\n *** --- obtain the current network information --- *** \n\n" >> output.txt
-echo -e "\n\n ** execute  ifconfig \n\n" >> output.txt | ifconfig  >> output.txt
+echo -e "\n\n *** --- obtain the current network information --- *** \n\n" >> $todayoutput
+echo -e "\n\n ** execute  ifconfig \n\n" >> $todayoutput | ifconfig  >> $todayoutput
 wait
-echo -e "\n\n ** execute  netstat -v -a \n\n" >> output.txt | netstat -v -a >> output.txt
+echo -e "\n\n ** execute  netstat -v -a \n\n" >> $todayoutput | netstat -v -a >> $todayoutput
 wait
-echo -e "\n\n ** execute  netstat -anlp  \n\n" >> output.txt | netstat -anlp >> output.txt
+echo -e "\n\n ** execute  netstat -anlp  \n\n" >> $todayoutput | netstat -anlp >> $todayoutput
 wait
-echo -e "\n\n ** execute  ss -pem \n\n" >> output.txt | ss -pem >> output.txt
+echo -e "\n\n ** execute  ss -pem \n\n" >> $todayoutput | ss -pem >> $todayoutput
 wait
 
 #commands to obtaine process and memory utilization
-echo -e "\n\n *** --- obtain the current process and memory information --- *** \n\n" >> output.txt
+echo -e "\n\n *** --- obtain the current process and memory information --- *** \n\n" >> $todayoutput
 
-echo -e "\n\n ** execute  top -n 3 \n\n" >> output.txt | top -n 3 >> output.txt
+echo -e "\n\n ** execute  top -n 3 \n\n" >> $todayoutput | top -n 3 >> $todayoutput
 wait
-echo -e "\n\n ** execute  ps aux \n\n" >> output.txt | ps aux >> output.txt
+echo -e "\n\n ** execute  ps aux \n\n" >> $todayoutput | ps aux >> $todayoutput
 wait
-echo -e "\n\n ** execute  vmstat -wat 5 3 \n\n" >> output.txt | vmstat -wat 5 3 >> output.txt
+echo -e "\n\n ** execute  vmstat -wat 5 3 \n\n" >> $todayoutput | vmstat -wat 5 3 >> $todayoutput
 wait
-echo -e "\n\n ** execute  iostat -cdhx 5 3 \n\n" >> output.txt | iostat -cdhx 5 3 >> output.txt
+echo -e "\n\n ** execute  iostat -cdhx 5 3 \n\n" >> $todayoutput | iostat -cdhx 5 3 >> $todayoutput
 wait
-echo -e "\n\n ** execute  systemctl  -l --show-type --all --no-pager  \n\n" >> output.txt | systemctl  -l --show-type --all --no-pager >> output.txt
+echo -e "\n\n ** execute  systemctl  -l --show-type --all --no-pager  \n\n" >> $todayoutput | systemctl  -l --show-type --all --no-pager >> $todayoutput
 wait
 #commands to obtain disk related information
-echo -e "\n\n  *** --- obtain the current disk utilization information --- *** \n\n" >> output.txt
-echo -e "\n\n ** execute  df -h \n\n" >> output.txt | df -h >> output.txt
+echo -e "\n\n  *** --- obtain the current disk utilization information --- *** \n\n" >> $todayoutput
+echo -e "\n\n ** execute  df -h \n\n" >> $todayoutput | df -h >> $todayoutput
 wait
-echo -e "\n\n ** execute  find /var/ -type f -exec du -Sh {} + | sort -rh | head -n 10 \n\n" >> output.txt | find /var/ -type f -exec du -Sh {} + | sort -rh | head -n 10 >> output.txt
+echo -e "\n\n ** execute  find /var/ -type f -exec du -Sh {} + | sort -rh | head -n 10 \n\n" >> $todayoutput | find /var/ -type f -exec du -Sh {} + | sort -rh | head -n 10 >> $todayoutput
 wait
-echo -e "\n\n ** execute  find /tmp/ -type f -exec du -Sh {} + | sort -rh | head -n 10 \n\n" >> output.txt | find /tmp/ -type f -exec du -Sh {} + | sort -rh | head -n 10 >> output.txt
+echo -e "\n\n ** execute  find /tmp/ -type f -exec du -Sh {} + | sort -rh | head -n 10 \n\n" >> $todayoutput | find /tmp/ -type f -exec du -Sh {} + | sort -rh | head -n 10 >> $todayoutput
 wait
-echo -e "\n\n ** execute  du -a /tmp | sort -n -r | head -n 10 \n\n" >> output.txt | du -a /tmp | sort -n -r | head -n 10 >> output.txt
+echo -e "\n\n ** execute  du -a /tmp | sort -n -r | head -n 10 \n\n" >> $todayoutput | du -a /tmp | sort -n -r | head -n 10 >> $todayoutput
 wait
-echo -e "\n\n ** execute  du -a /var | sort -n -r | head -n 10 \n\n" >> output.txt | du -a /var | sort -n -r | head -n 10 >> output.txt
+echo -e "\n\n ** execute  du -a /var | sort -n -r | head -n 10 \n\n" >> $todayoutput | du -a /var | sort -n -r | head -n 10 >> $todayoutput
 
 wait
 
 
-echo -e "\n\n *** --- script completed  --- *** \n\n" >> output.txt | clock >> output.txt
-echo -e script is completed and placed as output.txt
+echo -e "\n\n *** --- script completed  --- *** \n\n" >> $todayoutput | clock >> $todayoutput
+echo -e script is completed and placed as $todayoutput
